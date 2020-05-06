f1c2b098c4b963be6160ea2870a3682b2ef3acd5
Jenocn <544673717@qq.com>
2020-05-06 11:57:38 +0800

    增加vs工程

---
+++ peak-vs-project/peak-exec-win/main.cpp
@@ -0,0 +1,17 @@
+
+#include "VirtualMachine.h"
+
+#pragma comment(lib, "libpeak")
+
+using namespace peak;
+
+int main(int argc, char** argv) {
+	for (int i = 1; i < argc; ++i) {
+		auto journey = VirtualMachine::LoadFile(argv[i]);
+		if (journey) {
+			journey->Execute();
+		}
+	}
+	getchar();
+	return 0;
+}
